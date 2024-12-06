ce22e7863e0a8ee297a05821dd96dc2a538d7048
Your Name <your-email@example.com>
2024-12-06 11:44:32 +0200

    Видалив непотрібні рецепти, додав функціонал сортування рецептів за категоріями

--- index.js
+++ index.js
@@ -10,6 +10,40 @@ hearts.forEach(function(heart) {
         }
     });
 });
+// Обирання категорії рецепту
+const blocks = document.querySelectorAll('.categories');
+const elements = document.querySelectorAll('.content');
+
+// Функція для оновлення видимості елементів
+function updateVisibility(group) {
+    // Спочатку приховуємо всі елементи
+    elements.forEach(element => {
+        element.classList.remove('visible-recipe');
+        element.classList.add('hidden-recipe');
+    });
+
+    // Виконуємо з затримкою, щоб створити ефект оновлення
+    setTimeout(() => {
+        elements.forEach(element => {
+            if (group === 'all' || element.dataset.group === group) {
+                element.classList.remove('hidden-recipe');
+                element.classList.add('visible-recipe');
+            }
+        });
+    }, 50); // Час затримки в мілісекундах
+}
+
+// Початкове встановлення видимості
+updateVisibility('all');
+
+// Додати обробник подій для кожного блоку
+blocks.forEach(block => {
+    block.addEventListener('click', () => {
+        blocks.forEach(b => b.classList.remove('selected'));
+        block.classList.add('selected'); // Робимо обраний блок активним
+        updateVisibility(block.dataset.show); // Оновлюємо видимість елементів
+    });
+});
 // Відкриття/закриття рецепту
 const buttons = document.querySelectorAll(".recipe-button");
 const recipes = document.querySelectorAll(".recipe-wrapper");
@@ -17,7 +51,7 @@ const recipes = document.querySelectorAll(".recipe-wrapper");
 
 buttons.forEach(function(button) {
     button.addEventListener("click", function() {
-        const recipeId = button.getAttribute("data");
+        const recipeId = button.getAttribute("data-id");
         const recipe = document.getElementById(recipeId);
 
         recipe.classList.add('show');
