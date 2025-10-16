bf6c5eae1da2d1bbdab14773dcaa503beb5af71f
postolnyidavyd <postolnyi.davyd@student.uzhnu.edu.ua>
2025-10-16 11:28:43 +0300

    "невеликі зміни"

--- CooksBook/eslint.config.js
+++ CooksBook/eslint.config.js
@@ -6,7 +6,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**', 'cypress/**', 'backend/**'],
+    ignores: ['dist/**', 'node_modules/**', 'cypress/**', 'backend/**', 'husky/**'],
   },
 
   //Базові правила ESLint
