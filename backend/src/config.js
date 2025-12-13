45e180d9cce595ed5d0d8d5000f858deb859691e
postolnyidavyd <postolnyi.davyd@student.uzhnu.edu.ua>
2025-12-13 22:16:31 +0200

    Налаштування докеру та підготовка до деплою\хостингу

---
+++ backend/src/config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+module.exports = {
+  PORT: process.env.PORT || 4000,
+  MONGO_URL: process.env.MONGO_URL,
+  FRONTEND_ORIGIN: process.env.FRONTEND_ORIGIN,
+  IS_PROD: process.env.NODE_ENV === 'production',
+
+  ACCESS_EXPIRES: '15m',
+  REFRESH_EXPIRES: '30d'
+}
